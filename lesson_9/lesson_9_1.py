f11f357b476d11f481b2c3969db5e5f890c2016f
AlexVazzz <sashavazhov@gmail.com>
2020-12-14 03:51:34 +0200

    dorabotannoe from seriy karenov

--- lesson_9/lesson_9_1.py
+++ lesson_9/lesson_9_1.py
@@ -1,12 +1,14 @@
+import math
 number_of_flat = int(input('Введите номер квартиры \n'))
 etazh = int(input('Введи количество этажей \n'))
 kol_vo_kvartir = int(input('Введи количество квартир на этаже \n'))
 kvartir_v_padike = etazh * kol_vo_kvartir
-if number_of_flat%kvartir_v_padike ==0:
+if number_of_flat % kvartir_v_padike ==0:
     nomer_padika = number_of_flat // kvartir_v_padike
 else:
-    nomer_padika = number_of_flat // kvartir_v_padike + 1
+    nomer_padika = number_of_flat // kvartir_v_padike +1
 
-etazh = (number_of_flat - (nomer_padika - 1) * kvartir_v_padike + 1) // kol_vo_kvartir
+etazh = ((number_of_flat - (nomer_padika - 1) * kvartir_v_padike) / kol_vo_kvartir)
+s = math.ceil(etazh)
 
-print('тебе нужен падик номер ', nomer_padika, 'и этаж номер ', etazh)
+print('тебе нужен падик номер ', nomer_padika, 'и этаж номер ', s)
